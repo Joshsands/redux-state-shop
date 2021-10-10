ffd18f20d638e70f889bc6f2a9e3c8f1db0663c2
Joshua Sands <josh.sands@mail.com>
2021-10-10 17:01:50 -0500

    fixed redux reducers

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,4 +1,5 @@
-import { useReducer } from "react";
+import { combineReducers } from 'redux';
+
 import {
   UPDATE_PRODUCTS,
   ADD_TO_CART,
@@ -85,6 +86,4 @@ export const reducer = (state, action) => {
   }
 };
 
-export function useProductReducer(initialState) {
-  return useReducer(reducer, initialState)
-}
+export const rootReducer = combineReducers({ reducer });
